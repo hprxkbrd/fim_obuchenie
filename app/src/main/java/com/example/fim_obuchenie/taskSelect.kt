a8d04b8db606b485f86210c82ed5726dc4b58bf7
hprxkbrd <egor13ni@gmail.com>
2024-12-12 21:03:36 +0300

    Create taskSelect.kt

---
+++ app/src/main/java/com/example/fim_obuchenie/taskSelect.kt
@@ -0,0 +1,15 @@
+package com.example.fim_obuchenie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class taskSelect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+
+    }
+}
