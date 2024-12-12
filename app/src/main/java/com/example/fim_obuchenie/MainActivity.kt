e4ccffb71186dd712184c5726fc893b5f83fd4e3
hprxkbrd <egor13ni@gmail.com>
2024-12-12 23:16:21 +0300

    added button to transfer to lelvel select activity

--- app/src/main/java/com/example/fim_obuchenie/MainActivity.kt
+++ app/src/main/java/com/example/fim_obuchenie/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fim_obuchenie
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,5 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_lvlSlct = findViewById<Button>(R.id.button3)
+        btn_lvlSlct.setOnClickListener {
+            setContentView(R.layout.activity_lvl_select)
+        }
     }
 }
