0c4c7040c0c13d1aa1a87b8edba67814599a5f68
hprxkbrd <egor13ni@gmail.com>
2024-12-12 22:39:47 +0300

    activity_main hooked

--- app/src/main/java/com/example/fim_obuchenie/level_selectActivity.kt
+++ app/src/main/java/com/example/fim_obuchenie/level_selectActivity.kt
@@ -1,19 +1,7 @@
 package com.example.fim_obuchenie
 
-import android.content.Context
-import android.graphics.Canvas
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import android.graphics.Paint
-import android.graphics.Rect
-import android.util.AttributeSet
-import android.util.DisplayMetrics
-import android.view.View
-import android.view.WindowManager
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import android.widget.TextView
 
 class level_selectActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
